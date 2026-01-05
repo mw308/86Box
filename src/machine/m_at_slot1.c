0465cc1c1aab6884881afe2c8f77024982ef14d8
mw308 <34479591+mw308@users.noreply.github.com>
2026-01-05 23:53:49 +0000

    Added PB Maui+

--- src/machine/m_at_slot1.c
+++ src/machine/m_at_slot1.c
@@ -375,6 +375,41 @@ machine_at_p6i440e2_init(const machine_t *model)
 
     return ret;
 }
+int
+machine_at_pbmauip_init(const machine_t *model)
+{
+    int ret;
+
+    ret = bios_load_linear_combined2("roms/machines/pbmauiplus/P11-0012.BIO",
+                                     "roms/machines/pbmauiplus/P11-0012.BI1",
+                                     "roms/machines/pbmauiplus/P11-0012.BI2",
+                                     "roms/machines/pbmauiplus/P11-0012.BI3",
+                                     "roms/machines/pbmauiplus/BIOS.REC",
+                                     0x3a000, 128);
+
+    if (bios_only || !ret)
+        return ret;
+
+    machine_at_common_init_ex(model, 2);
+
+    pci_init(PCI_CONFIG_TYPE_1);
+    pci_register_slot(0x00, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 0);
+    pci_register_slot(0x07, PCI_CARD_SOUTHBRIDGE, 1, 2, 3, 4);
+    pci_register_slot(0x09, PCI_CARD_NORMAL,      1, 2, 3, 4);
+    pci_register_slot(0x0A, PCI_CARD_NORMAL,      2, 3, 4, 1);
+    pci_register_slot(0x01, PCI_CARD_AGPBRIDGE,   1, 2, 3, 4);
+
+    device_add(&i440ex_device);
+    device_add(&piix4_device);
+    device_add_params(&w83977_device, (void *) (W83977TF | W83977_AMI | W83977_NO_NVR));
+    device_add(&sst_flash_29ee010_device);
+    spd_register(SPD_TYPE_SDRAM, 0x03, 256);
+    device_add(&w83781d_device);       /* fans: CPU, CHS, PS; temperatures: unused, CPU, System */
+    hwm_values.temperatures[0] = 0;    /* unused */
+    hwm_values.voltages[1]     = 1500; /* CPUVTT */
+
+    return ret;
+}
 
 /* i440BX */
 int
